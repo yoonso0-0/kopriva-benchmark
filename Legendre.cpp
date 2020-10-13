f884457aff7a0c6ed80e2283e3998de73ed5c2cc
Yoonsoo <kimys94@snu.ac.kr>
2020-10-13 21:24:42 +0900

    Rearranged file & test template function

--- Legendre.cpp
+++ Legendre.cpp
@@ -6,79 +6,7 @@
 
 using namespace constants;
 
-void legendre_pt_val_and_deriv(int N, double x, double& f, double& df, int& ierr)
-{
-/* 
- *  Evaluates P_N(x) and P'_N(x) for a given N and x 
- *  
- *  Parameters
- *  ----------
- *  Input, int N : order of polynomial
- * 
- *  Input, double X : point at which P_N, P'_N are evaluated
- * 
- *  Output, double f : P_N(x)
- * 
- *  Output, double df : P'_N(x)
- * 
- *  Output, int ierr : error flag
- *      0, no errors
- *      -1, x is out of domain [-1, 1]
- *      -2, N is negative
- * 
- */
-    if (std::abs(x) > 1.0)
-    {
-        ierr = -1;
-        std::cout << "legendre_pt_val_and_deriv - abs(X) > 1.0" << std::endl;
-        return;
-    }
-    if (N==0)
-    {
-        f = 1.;
-        df = 0.;
-        return;
-    }
-    else if (N==1)
-    {
-        f = x;
-        df = 1.;
-        return;
-    }
-    else if (N<0)
-    {
-        ierr = -2;
-        std::cout << "legendre_pt_val_and_deriv - N < 0" << std::endl;
-        return;
-    }
-    else
-    {
-        double lkm2 {1.};
-        double lkm1 {x};
-        double dlkm2 {0.};
-        double dlkm1 {1.};
-
-        double lk {0.};
-        double dlk {0.};
-
-        for (int k = 2; k <= N; k++)
-        {
-            lk = ( lkm1 * x * (2 * k - 1) - lkm2 * (k - 1) ) / k ;
-            dlk = dlkm2 + (2 * k - 1) * lkm1 ;
-            
-            // store current values for next step
-            lkm2 = lkm1;
-            lkm1 = lk;
-            dlkm2 = dlkm1;
-            dlkm1 = dlk;
-        }
-        ierr = 0;
-        f = lk;
-        df = dlk;
-    }
-}
-
-void findroot_legendre_newton(int N, double xi, double& x, int& ierr)
+void legendre_findroot_newton(int N, double xi, double& x, int& ierr)
 {
 /*
  *  Solve P_N(x0) = 0 for a given N via Newton's method
@@ -125,4 +53,73 @@ void findroot_legendre_newton(int N, double xi, double& x, int& ierr)
     ierr = -1;
     return;
 
+}
+
+void legendre_pt_val_and_deriv(int N, double x, double& f, double& df, int& ierr)
+{
+/* 
+ *  Evaluates P_N(x) and P'_N(x) for a given N and x 
+ *  
+ *  Parameters
+ *  ----------
+ *  Input, int N : order of polynomial
+ *  Input, double X : point at which P_N, P'_N are evaluated
+ *  Output, double f : P_N(x)
+ *  Output, double df : P'_N(x)
+ *  Output, int ierr : error flag
+ *      0, no errors
+ *      -1, x is out of domain [-1, 1]
+ *      -2, N is negative
+ * 
+ */
+
+    if (std::abs(x) > 1.0)
+    {
+        ierr = -1;
+        std::cout << "legendre_pt_val_and_deriv - abs(X) > 1.0" << std::endl;
+        return;
+    }
+    else if (N<0)
+    {
+        ierr = -2;
+        std::cout << "legendre_pt_val_and_deriv - N < 0" << std::endl;
+        return;
+    }
+    else if (N==0)
+    {
+        f = 1.;
+        df = 0.;
+        return;
+    }
+    else if (N==1)
+    {
+        f = x;
+        df = 1.;
+        return;
+    }
+    else
+    {
+        double lkm2 {1.};
+        double lkm1 {x};
+        double dlkm2 {0.};
+        double dlkm1 {1.};
+
+        double lk {0.};
+        double dlk {0.};
+
+        for (int k = 2; k <= N; k++)
+        {
+            lk = ( lkm1 * x * (2 * k - 1) - lkm2 * (k - 1) ) / k ;
+            dlk = dlkm2 + (2 * k - 1) * lkm1 ;
+            
+            // store current values for next step
+            lkm2 = lkm1;
+            lkm1 = lk;
+            dlkm2 = dlkm1;
+            dlkm1 = dlk;
+        }
+        ierr = 0;
+        f = lk;
+        df = dlk;
+    }
 }
--- Legendre.hpp
+++ Legendre.hpp
@@ -1,7 +1,18 @@
 #ifndef LEGENDRE
 #define LEGENDRE
 
+template <typename T, size_t N>
+void legendre_gauss_nodes_weights(T (&nodes)[N], T (&weights)[N])
+{
+    std::cout << "template function !!" << std::endl;
+    return;
+    
+    // Chebyshev Gauss points (Kopriva p.63)
+    // ...
+
+}
+
+void legendre_findroot_newton(int N, double xi, double& x, int& ierr);
 void legendre_pt_val_and_deriv(int N, double x, double& f, double& df, int& ierr);
-void findroot_legendre_newton(int N, double xi, double& x, int& ierr);
 
 #endif
