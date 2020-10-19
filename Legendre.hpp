515855a84a9fc573fcb158aa6ebab6268cde4bcb
Yoonsoo <kimys94@snu.ac.kr>
2020-10-19 16:03:01 +0900

    Benchmark of Gauss nodes (Table3.1)

--- Legendre.hpp
+++ Legendre.hpp
@@ -1,18 +1,84 @@
-#ifndef LEGENDRE
-#define LEGENDRE
+#include <array>
+#include <cmath>
+#include <assert.h>
+
+#include "Constants.hpp"
+
+#pragma once
+
+void legendre_findroot_newton(const int polynomial_degree, const double x_init, double& x_root);
+void legendre_pt_val_and_deriv(const int polynomial_degree, const double x_eval, double& f, double& df);
+
+template <typename T, const size_t N>
+void legendre_gauss_nodes_weights(std::array<T, N>& nodes, std::array<T, N>& weights)
+{
+/*
+ *  Calculate gauss points and quadrature weights
+ *  
+ *  Parameters
+ *  ----------
+ *  Output, array<double, N> nodes : Gauss-Legendre quadrature points (degree : N)
+ *  Output, array<double, N> weights : quadrature weights (degree : N)
+ */
+    assert( N >= 1 && "legendre_gauss_nodes_weights - input array has zero length");
+    
+    if (N==1)
+    {
+        nodes[0] = 0.0;
+        weights[0] = 2.0;
+        return;
+    }
+    else if (N==2)
+    {
+        nodes[0] = -1.0/sqrt(3.);
+        nodes[1] = -nodes[0];
+        weights[0] = 1.0;
+        weights[1] = weights[0];
+        return;
+    }
+    else
+    {
+        double delta_x;
+        double Lx;
+        double dLx;
+
+        for (size_t j = 0; j < N/2; j++){
+
+            // initial guess - Chebyshev Gauss points (Kopriva p.63)
+            double x_j = - cos( M_PI * (2*j+1) / (2*N) );
+        
+            for(size_t i_root = 0; i_root <= constants::nmax_iter_rootsolving ; i_root++)
+            {
+                legendre_pt_val_and_deriv(N, x_j, Lx, dLx);
+
+                delta_x = - Lx / dLx;
+                x_j = x_j + delta_x;
+
+                // std::cout << "iter = " << i_root << "\t x = " << x_j << "\t fx = " << Lx << std::endl;
+
+                if ( std::abs(delta_x) < constants::epsilon_double )
+                {
+                    break;
+                }
+            }
+            nodes[j] = x_j;
+            nodes[N-1-j] = - nodes[j];
+            weights[j] = 2.0 / ((1.0 - pow(x_j, 2.0)) * pow(dLx, 2.0));
+            weights[N-1-j] = weights[j];
+        }
+        if (N % 2) {
+            legendre_pt_val_and_deriv(N, 0., Lx, dLx);
+            nodes[N/2] = 0.0;
+            weights[N/2] = 2.0 / pow(dLx, 2.0);
+        }
+        return;
+    }
+
+    assert(0 && "You should not reach here!");
+} // void legendre_gauss_nodes_weights
 
 template <typename T, size_t N>
-void legendre_gauss_nodes_weights(T (&nodes)[N], T (&weights)[N])
+void legendre_gauss_lobatto_nodes_weights(std::array<T, N>, std::array<T, N>)
 {
-    std::cout << "template function !!" << std::endl;
-    return;
-    
-    // Chebyshev Gauss points (Kopriva p.63)
     // ...
-
 }
-
-void legendre_findroot_newton(int N, double xi, double& x, int& ierr);
-void legendre_pt_val_and_deriv(int N, double x, double& f, double& df, int& ierr);
-
-#endif
--- test.cpp
+++ test.cpp
@@ -1,45 +1,65 @@
 #include <iostream>
+#include <iomanip>
 
 #include "Constants.hpp"
 #include "Legendre.hpp"
 
-// 
-void test_legendre_findroot_newton();
+void benchmark_gauss_nodes_weights();
 void test_legendre_pt_val_and_deriv();
+
 
 // =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+= //
 //                                     MAIN
 // =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+= //
 int main(int argc, char const *argv[])
 {
-    
     // test_legendre_pt_val_and_deriv();
     // test_legendre_findroot_newton();
 
-    int a[3] {1,2,3};
-    legendre_gauss_nodes_weights(a, a);
+    benchmark_gauss_nodes_weights();
+    
 
     return 0;
 }
 
 // -------------------------------- TEST ROUTINES -------------------------------- //
-void test_legendre_findroot_newton()
-{
-    std::cout << "* ------ TEST : Root of Pn(x) ------ *" << std::endl;
-    int stat;
-    double root;
-    legendre_findroot_newton(7, -0.004, root, stat);
-    std::cout << "root = " << root << std::endl;
-    std::cout << "stat = " << stat << std::endl;
-}
+// void test_legendre_findroot_newton()
+// {
+//     std::cout << "* ------ TEST : Root of Pn(x) ------ *" << std::endl;
+//     double root;
+//     legendre_findroot_newton(7, -0.4, root);
+//     std::cout << "root = " << root << std::endl;
+// }
 
 void test_legendre_pt_val_and_deriv()
 {
     std::cout << "* ------ TEST : P_N(x) ------ *" << std::endl;
-    int stat;
     double f, df;
-    legendre_pt_val_and_deriv(4, 0.4, f, df, stat);
+    legendre_pt_val_and_deriv(0, 0.4, f, df);
 
     std::cout << f << std::endl;
     std::cout << df << std::endl;
+}
+
+void benchmark_gauss_nodes_weights()
+{
+    // 
+    // Benchmark for N+1=7 (Kopriva p.67 Table 3.1)
+    // 
+    std::array<double, 7> nodes;
+    std::array<double, 7> weights;
+    legendre_gauss_nodes_weights(nodes, weights);
+
+    // output formatting
+    std::cout << std::setprecision(15);
+    std::cout << std::fixed;
+    std::cout << std::setw(3) << "j" << std::setw(20) << "x_j" << std::setw(20) << "w_j" << std::endl;
+
+    for (size_t i=0; i<4; i++)
+    {
+        std::cout << std::setw(3) << i 
+                  << std::setw(20) << nodes.at(i)
+                  << std::setw(20) << weights.at(i)
+                  << std::endl;
+    }
 }
