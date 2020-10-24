1dc9e2603c504ba127fcc87c54a94c984cd79213
Yoonsoo <kimys94@snu.ac.kr>
2020-10-24 21:48:24 +0900

    Legendre nodes and weights & Benchmark result

---
+++ NodesAndWeights.hpp
@@ -0,0 +1,141 @@
+#include <cmath>
+#include <assert.h>
+
+#pragma once
+
+namespace legendre{
+
+template <typename T, size_t N>
+void gauss_nodes_and_weights(std::array<T, N>& nodes, std::array<T, N>& weights)
+{
+/* 
+ *  Calculate Gauss points and quadrature weights
+ *  (Kopriva Algorithm 23)
+ *  
+ *  Parameters
+ *  ----------
+ *  Output (implicit), array<double, N> nodes : Gauss quadrature points (degree : N)
+ *  Output (implicit), array<double, N> weights : quadrature weights (degree : N)
+ * 
+ */
+    assert( N >= 1 && "legendre::gauss_nodes_weights - input array has zero length");
+    
+    if (N==1)
+    {
+        nodes[0] = 0.0;
+        weights[0] = 2.0;
+        return;
+    }
+    else if (N==2)
+    {
+        nodes[0] = -1.0/sqrt(3.);
+        weights[0] = 1.0;
+        nodes[1] = -nodes[0];
+        weights[1] = weights[0];
+        return;
+    }
+    else
+    {
+        double delta_x;
+        double Lx;
+        double dLx;
+
+        for (size_t j = 0; j < N/2; j++)
+        {
+            // initial guess - Chebyshev Gauss points
+            double x_j = - cos( M_PI * (2*j + 1) / (2*N) );
+        
+            // find root via Newton-Raphson method
+            for(size_t i_root = 0; i_root <= constants::nmax_iter_rootsolving ; i_root++)
+            {
+                legendre::polynomial_and_derivative(N, x_j, Lx, dLx);
+                delta_x = - Lx / dLx;
+                x_j = x_j + delta_x;
+
+                if ( std::abs(delta_x) < constants::epsilon_double ) break;
+            }
+            legendre::polynomial_and_derivative(N, x_j, Lx, dLx);
+            nodes[j] = x_j;
+            nodes[N-1-j] = - nodes[j];
+            weights[j] = 2.0 / ((1.0 - pow(x_j, 2.0)) * pow(dLx, 2.0));
+            weights[N-1-j] = weights[j];
+        }
+
+        // if polynomial degree N is odd, manually assign weight for x=0 node
+        if (N % 2) {
+            legendre::polynomial_and_derivative(N, 0., Lx, dLx);
+            nodes[N/2] = 0.0;
+            weights[N/2] = 2.0 / pow(dLx, 2.0);
+        }
+        return;
+    }
+} // void legendre_gauss_nodes_weights
+
+
+template <typename T, size_t N>
+void gauss_lobatto_nodes_and_weights(std::array<T, N>& nodes, std::array<T, N>& weights)
+{
+/*
+ *  Calculate Gauss-Lobatto points and quadrature weights
+ *  (Kopriva Algorithm 25)
+ *  
+ *  Parameters
+ *  ----------
+ *  Output (implicit), array<double, N> nodes : Gauss-Lobatto quadrature points (degree : N)
+ *  Output (implicit), array<double, N> weights : quadrature weights (degree : N)
+ * 
+ */
+    assert( N >= 2 && "legendre::gauss_lobatto_nodes_weights - input array length must be >= 2");
+
+    if (N==2)
+    {
+        nodes[0] = -1.0;
+        nodes[1] =  1.0;
+        weights[0] = 1.0;
+        weights[1] = weights[0];
+    }
+    else
+    {
+        // endpoints x = -1.0, x = 1.0
+        nodes[0] = -1.0;
+        nodes[N-1] =  1.0;
+        weights[0] = 2.0 / (N*(N-1));
+        weights[N-1] = weights[0];
+
+        double delta_x;
+        double q_x;
+        double dq_x;
+        double L_x;
+
+        for (size_t j = 1; j < N/2; j++)
+        {
+            // initial guess (Kopriva eq 3.7)
+            double x_j = - cos( ( M_PI * (j + 0.25) - 3.0 / ( 8.0 * M_PI * (j + 0.25) ) ) / (N - 1) );
+
+            // find root via Newton-Raphson method
+            for(size_t i_root = 0; i_root <= constants::nmax_iter_rootsolving ; i_root++)
+            {
+                legendre::q_and_L(N-1, x_j, q_x, dq_x, L_x);
+                delta_x = - q_x / dq_x;
+                x_j = x_j + delta_x;
+
+                if ( std::abs(delta_x) < constants::epsilon_double ) break;
+            }
+            legendre::q_and_L(N-1, x_j, q_x, dq_x, L_x);
+            nodes[j] = x_j;
+            nodes[N-1-j] = - nodes[j];
+            weights[j] = 2.0 / ( N * (N-1) * pow(L_x, 2.0) );
+            weights[N-1-j] = weights[j];
+        }
+
+        // if polynomial degree N is odd, manually assign weight for x=0 node
+        if (N % 2) {
+            legendre::q_and_L(N-1, 0.0, q_x, dq_x, L_x);
+            nodes[N/2] = 0.0;
+            weights[N/2] = 2.0 / ( N * (N-1) * pow(L_x, 2.0) );
+        }
+        return;
+    }
+} // void gauss_lobatto_nodes_weights
+
+} // namespace legendre
--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 
 #include "Constants.hpp"
 #include "Legendre.hpp"
+#include "NodesAndWeights.hpp"
 
 void benchmark_gauss_nodes_weights();
 void test_legendre_pt_val_and_deriv();
@@ -14,28 +15,18 @@ void test_legendre_pt_val_and_deriv();
 int main(int argc, char const *argv[])
 {
     // test_legendre_pt_val_and_deriv();
-    // test_legendre_findroot_newton();
-
     benchmark_gauss_nodes_weights();
-    
 
     return 0;
 }
 
 // -------------------------------- TEST ROUTINES -------------------------------- //
-// void test_legendre_findroot_newton()
-// {
-//     std::cout << "* ------ TEST : Root of Pn(x) ------ *" << std::endl;
-//     double root;
-//     legendre_findroot_newton(7, -0.4, root);
-//     std::cout << "root = " << root << std::endl;
-// }
 
 void test_legendre_pt_val_and_deriv()
 {
     std::cout << "* ------ TEST : P_N(x) ------ *" << std::endl;
     double f, df;
-    legendre_pt_val_and_deriv(0, 0.4, f, df);
+    legendre::polynomial_and_derivative(0, 0.4, f, df);
 
     std::cout << f << std::endl;
     std::cout << df << std::endl;
@@ -46,20 +37,44 @@ void benchmark_gauss_nodes_weights()
     // 
     // Benchmark for N+1=7 (Kopriva p.67 Table 3.1)
     // 
-    std::array<double, 7> nodes;
-    std::array<double, 7> weights;
-    legendre_gauss_nodes_weights(nodes, weights);
+    const size_t benchmark_degree = 7;
+    std::cout << " < Benchmark for N=" << benchmark_degree-1 << " (Kopriva p.67 Table 3.1) > " << std::endl;
+    
+    std::array<double, benchmark_degree> nodes;
+    std::array<double, benchmark_degree> weights;
 
-    // output formatting
+    // print out results up to 15-th precision
     std::cout << std::setprecision(15);
     std::cout << std::fixed;
-    std::cout << std::setw(3) << "j" << std::setw(20) << "x_j" << std::setw(20) << "w_j" << std::endl;
 
-    for (size_t i=0; i<4; i++)
+    // 
+    // Gauss points
+    // 
+    std::cout << " * Gauss" << std::endl;
+    legendre::gauss_nodes_and_weights(nodes, weights);
+
+    std::cout << std::setw(3) << "j" << std::setw(20) << "x_j" << std::setw(20) << "w_j" << std::endl;
+    for (size_t i = 0; i < benchmark_degree/2 + 1; i++)
     {
         std::cout << std::setw(3) << i 
                   << std::setw(20) << nodes.at(i)
                   << std::setw(20) << weights.at(i)
                   << std::endl;
     }
+    
+    // 
+    // Gauss-Lobatto points
+    // 
+    std::cout << " * Gauss-Lobatto" << std::endl;
+    legendre::gauss_lobatto_nodes_and_weights(nodes, weights);
+    
+    std::cout << std::setw(3) << "j" << std::setw(20) << "x_j" << std::setw(20) << "w_j" << std::endl;
+    for (size_t i = 0; i < benchmark_degree/2 + 1; i++)
+    {
+        std::cout << std::setw(3) << i 
+                  << std::setw(20) << nodes.at(i)
+                  << std::setw(20) << weights.at(i)
+                  << std::endl;
+    }
+
 }
