17d3819baef59f58979cd4db3f06e1518863b764
Yoonsoo <kimys94@snu.ac.kr>
2020-10-24 21:46:22 +0900

    Added legendre:: namespace and q_and_L routine

--- Legendre.cpp
+++ Legendre.cpp
@@ -5,14 +5,17 @@
 #include "Legendre.hpp"
 #include "Constants.hpp"
 
-void legendre_pt_val_and_deriv(const int poly_degree_N, const double x_eval, double& L_x, double& dL_x)
+namespace legendre{
+
+void polynomial_and_derivative(const int poly_degree_N, const double x_eval, double& L_x, double& dL_x)
 {
 /* 
- *  Evaluates L_N(x) and dL_N(x) for a given N and x 
+ *  Evaluates Legendre polynomial and its derivagive for given N and x
+ *  (Kopriva Algorithm 22)
  *  
  *  Parameters
  *  ----------
- *  Input, int poly_degree_N : order of polynomial
+ *  Input, int poly_degree_N : degree of Legendre polynomial
  *  Input, double x_eval : point at which L_N(x), dL_N(x) are evaluated
  *  Output (implicit), double L_x : L_N(x)
  *  Output (implicit), double dL_x : dL_N(x)
@@ -41,8 +44,8 @@ void legendre_pt_val_and_deriv(const int poly_degree_N, const double x_eval, dou
         double dL_k_minus_2 {0.};
         double dL_k_minus_1 {1.};
 
-        double L_k {0.};
-        double dL_k {0.};
+        double L_k {};
+        double dL_k {};
 
         for (size_t k = 2; k <= poly_degree_N; k++)
         {
@@ -59,4 +62,70 @@ void legendre_pt_val_and_deriv(const int poly_degree_N, const double x_eval, dou
         dL_x = dL_k;
         return;
     }
-}
+} // void polynomial_and_derivative
+
+void q_and_L(const int poly_degree_N, const double x_eval, double& q_x, double& dq_x, double& L_x)
+{
+/* 
+ *  Evaluates q, q', L_N(x) 
+ *  (Kopriva Algorithm 24)
+ *  
+ *  Parameters
+ *  ----------
+ *  Input, int poly_degree_N : degree of Legendre polynomial
+ *  Input, double x_eval : point at which q_x, dq_x, L_x are evaluated
+ *  Output (implicit), double q_x : q(x) = L_{N+1}(x) - L_{N-1}(x)
+ *  Output (implicit), double dq_x : dq(x) = dL_{N+1}(x) - dL_{N-1}(x)
+ *  Output (implicit), double L_x : L_N(x)
+ * 
+ */
+
+    assert( abs(x_eval) <= 1.0 && "legendre_q_and_L - abs(X) > 1.0");
+    assert( poly_degree_N >= 0 && "legendre_q_and_L - N < 0");
+
+    if (poly_degree_N==0)
+    {
+        L_x = 1.;
+        q_x = 0.;
+        dq_x = 0.;
+        return;
+    }
+    else if (poly_degree_N==1)
+    {
+        L_x = x_eval;
+        q_x = 1.;
+        dq_x = 0.;
+        return;
+    }
+    else
+    {
+
+        double L_k_minus_2 {1.};
+        double L_k_minus_1 {x_eval};
+        double dL_k_minus_2 {0.};
+        double dL_k_minus_1 {1.};
+
+        double L_k {};
+        double dL_k {};
+
+        for (size_t k = 2; k <= poly_degree_N; k++)
+        {
+            L_k = ( L_k_minus_1 * x_eval * (2 * k - 1) - L_k_minus_2 * (k - 1) ) / k ;
+            dL_k = dL_k_minus_2 + (2 * k - 1) * L_k_minus_1 ;
+            
+            // store current values for next step
+            L_k_minus_2 = L_k_minus_1;
+            L_k_minus_1 = L_k;
+            dL_k_minus_2 = dL_k_minus_1;
+            dL_k_minus_1 = dL_k;
+        }
+
+        // note that the value of L_{N-1}(x) is stored in L_k_minus_2 at this point
+        L_x = L_k;
+        q_x = (L_k * x_eval- L_k_minus_2) * (2 * poly_degree_N + 1) / (poly_degree_N + 1);
+        dq_x = dL_k_minus_2 + (2 * poly_degree_N + 1) * L_k - dL_k_minus_2;
+        return;
+    }
+} // void q_and_L
+
+} // namespace legendre
--- Legendre.hpp
+++ Legendre.hpp
@@ -1,84 +1,13 @@
 #include <array>
 #include <cmath>
-#include <assert.h>
 
 #include "Constants.hpp"
 
 #pragma once
 
-void legendre_findroot_newton(const int polynomial_degree, const double x_init, double& x_root);
-void legendre_pt_val_and_deriv(const int polynomial_degree, const double x_eval, double& f, double& df);
+namespace legendre{
 
-template <typename T, const size_t N>
-void legendre_gauss_nodes_weights(std::array<T, N>& nodes, std::array<T, N>& weights)
-{
-/*
- *  Calculate gauss points and quadrature weights
- *  
- *  Parameters
- *  ----------
- *  Output, array<double, N> nodes : Gauss-Legendre quadrature points (degree : N)
- *  Output, array<double, N> weights : quadrature weights (degree : N)
- */
-    assert( N >= 1 && "legendre_gauss_nodes_weights - input array has zero length");
-    
-    if (N==1)
-    {
-        nodes[0] = 0.0;
-        weights[0] = 2.0;
-        return;
-    }
-    else if (N==2)
-    {
-        nodes[0] = -1.0/sqrt(3.);
-        nodes[1] = -nodes[0];
-        weights[0] = 1.0;
-        weights[1] = weights[0];
-        return;
-    }
-    else
-    {
-        double delta_x;
-        double Lx;
-        double dLx;
+void polynomial_and_derivative(const int poly_degree_N, const double x_eval, double& L_x, double& dL_x);
+void q_and_L(const int poly_degree_N, const double x_eval, double& q_x, double& dq_x, double& L_x);
 
-        for (size_t j = 0; j < N/2; j++){
-
-            // initial guess - Chebyshev Gauss points (Kopriva p.63)
-            double x_j = - cos( M_PI * (2*j+1) / (2*N) );
-        
-            for(size_t i_root = 0; i_root <= constants::nmax_iter_rootsolving ; i_root++)
-            {
-                legendre_pt_val_and_deriv(N, x_j, Lx, dLx);
-
-                delta_x = - Lx / dLx;
-                x_j = x_j + delta_x;
-
-                // std::cout << "iter = " << i_root << "\t x = " << x_j << "\t fx = " << Lx << std::endl;
-
-                if ( std::abs(delta_x) < constants::epsilon_double )
-                {
-                    break;
-                }
-            }
-            nodes[j] = x_j;
-            nodes[N-1-j] = - nodes[j];
-            weights[j] = 2.0 / ((1.0 - pow(x_j, 2.0)) * pow(dLx, 2.0));
-            weights[N-1-j] = weights[j];
-        }
-        if (N % 2) {
-            legendre_pt_val_and_deriv(N, 0., Lx, dLx);
-            nodes[N/2] = 0.0;
-            weights[N/2] = 2.0 / pow(dLx, 2.0);
-        }
-        return;
-    }
-
-    assert(0 && "You should not reach here!");
-} // void legendre_gauss_nodes_weights
-
-template <typename T, size_t N>
-void legendre_gauss_lobatto_nodes_weights(std::array<T, N>, std::array<T, N>)
-{
-    // ...
-}
+} // namespace legendre
