63e01b2c93c69786eccb0e614b117627eb6fed4c
Yoonsoo <kimys94@snu.ac.kr>
2020-11-09 21:19:11 +0900

    Refactored test routines

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iomanip>
+#include <iostream>
+
+#include <boost/numeric/ublas/io.hpp>
+#include <boost/numeric/ublas/vector.hpp>
+
+// =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+// //
+//                                     MAIN
+// =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+// //
+int main() { return 0; }
--- test.cpp
+++ test.cpp
@@ -1,92 +1,14 @@
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 
-#include "Constants.hpp"
-#include "Legendre.hpp"
-#include "NodesAndWeights.hpp"
-#include "Interpolation/BarycentricWeights.hpp"
+#include "Tests/tests.hpp"
 
-void benchmark_gauss_nodes_weights();
-void test_legendre_pt_val_and_deriv();
+int main() {
+  tests::test_legendre_pt_val_and_deriv();
+  std::cout << "" << std::endl;
 
+  tests::test_gauss_nodes_weights();
+  std::cout << "" << std::endl;
 
-// =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+= //
-//                                     MAIN
-// =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+= //
-int main(int argc, char const *argv[])
-{
-    // test_legendre_pt_val_and_deriv();
-    // benchmark_gauss_nodes_weights();
-
-    std::array<double, 4> pts {-2, -1, 1, 2};
-    std::array<double, 4> weights;
-
-    interpolation::barycentric_weights(pts, weights);
-
-    for (size_t i = 0; i < 4; i++)
-    {
-        std::cout << pts[i] << "   " << weights[i] << std::endl;
-    }
-    
-
-    return 0;
-}
-
-// -------------------------------- TEST ROUTINES -------------------------------- //
-
-void test_legendre_pt_val_and_deriv()
-{
-    std::cout << "* ------ TEST : P_N(x) ------ *" << std::endl;
-    double f, df;
-    legendre::polynomial_and_derivative(0, 0.4, f, df);
-
-    std::cout << f << std::endl;
-    std::cout << df << std::endl;
-}
-
-void benchmark_gauss_nodes_weights()
-{
-    // 
-    // Benchmark for N+1=7 (Kopriva p.67 Table 3.1)
-    // 
-    const size_t benchmark_degree = 7;
-    std::cout << " < Benchmark for N=" << benchmark_degree-1 << " (Kopriva p.67 Table 3.1) > " << std::endl;
-    
-    std::array<double, benchmark_degree> nodes;
-    std::array<double, benchmark_degree> weights;
-
-    // print out results up to 15-th precision
-    std::cout << std::setprecision(15);
-    std::cout << std::fixed;
-
-    // 
-    // Gauss points
-    // 
-    std::cout << " * Gauss" << std::endl;
-    legendre::gauss_nodes_and_weights(nodes, weights);
-
-    std::cout << std::setw(3) << "j" << std::setw(20) << "x_j" << std::setw(20) << "w_j" << std::endl;
-    for (size_t i = 0; i < benchmark_degree/2 + 1; i++)
-    {
-        std::cout << std::setw(3) << i 
-                  << std::setw(20) << nodes.at(i)
-                  << std::setw(20) << weights.at(i)
-                  << std::endl;
-    }
-    
-    // 
-    // Gauss-Lobatto points
-    // 
-    std::cout << " * Gauss-Lobatto" << std::endl;
-    legendre::gauss_lobatto_nodes_and_weights(nodes, weights);
-    
-    std::cout << std::setw(3) << "j" << std::setw(20) << "x_j" << std::setw(20) << "w_j" << std::endl;
-    for (size_t i = 0; i < benchmark_degree/2 + 1; i++)
-    {
-        std::cout << std::setw(3) << i 
-                  << std::setw(20) << nodes.at(i)
-                  << std::setw(20) << weights.at(i)
-                  << std::endl;
-    }
-
+  tests::test_polynomial_derivative_matrix();
 }
