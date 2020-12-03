c617d338719e7b7464b80796117cb30ceb1b8243
Yoonsoo <kimys94@snu.ac.kr>
2020-12-03 22:14:42 +0900

    Riemann solver for 1D wave equation

---
+++ DG/RiemannSolver.hpp
@@ -0,0 +1,7 @@
+#include <boost/numeric/ublas/vector.hpp>
+
+#pragma once
+
+void riemann_solver(const ublas::vector<double> &u_boundary_internal,
+                    const ublas::vector<double> &u_boundary_external,
+                    const double normal_1d, ublas::vector<double> &numerical_flux);
