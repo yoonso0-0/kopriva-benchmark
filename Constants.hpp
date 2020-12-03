423f00a31675dcf77908c8d79c1e27db223e7e38
Yoonsoo <kimys94@snu.ac.kr>
2020-12-03 22:13:27 +0900

    Wave speed as constant

--- Constants.hpp
+++ Constants.hpp
@@ -8,6 +8,8 @@ namespace constants {
 constexpr double epsilon_double{1e-16};
 constexpr size_t nmax_iter_rootsolving{100};
 
+constexpr double wave_speed{1.0};
+
 // SSP-RK3 coefficients - Williamson's (Kopriva Table 4.1)
 constexpr double RK3_ams[3] = {0.0, -5.0 / 9, -153.0 / 128};
 constexpr double RK3_bms[3] = {0, 1.0 / 3, 3.0 / 4};
@@ -23,7 +25,8 @@ inline int AlmostEqual(double a, double b) {
             return 0;
         }
     } else {
-        if ((abs_diff < epsilon_double * abs(a)) and (abs_diff < epsilon_double * abs(b))) {
+        if ((abs_diff < epsilon_double * abs(a)) and
+            (abs_diff < epsilon_double * abs(b))) {
             return 1;
         } else {
             return 0;
